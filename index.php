81be4c0ac9f5b1dab2ce019831db20bfab4d6ad0
Yoomand11 <sonow292@gmail.com>
2025-03-05 21:06:31 +0700

    tambah fitur insert

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $dataMahasiswa = $mahasiswa->getAllMahasiswa();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa Sistem Informasi</title>
+    <title>Data Mahasiswa Sistem Informasi versi insert</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
