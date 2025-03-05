7f188184a8cf603e9aa54d5b64a95ba43de3c8b3
Yoomand11 <sonow292@gmail.com>
2025-03-05 21:04:22 +0700

    ganti title

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $dataMahasiswa = $mahasiswa->getAllMahasiswa();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+    <title>Data Mahasiswa Sistem Informasi</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
